3c14865251677407d1098d7ae9ed9f87decdb06d
Vanessa <vanessa.ferreira.dev@gmail.com>
2023-03-23 12:20:56 +0000

    About me drafts

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
+import nessaImage from '../../assets/Nessa5.png';
 import "./About.css"
 
 const About = () => {
   return (
     <section id="about" className="about-section custom-about">
-      <div className="container">
-        <h2 className="text-center">About Me</h2>
-        <section id="about" className="about-section custom-about">
-  <div className="container">
-    <h2 className="text-center">About Me</h2>
-    <img src="./assets/Nessa5.png" alt="About me" />
+    <div className="container">
+      <div className="image-wrapper">
+      <img src={nessaImage} alt="MyPicture" />
+    </div>
+    <div className="text-wrapper">
+      <h2>About Me</h2>
     <p>As a Frontend Developer with an MSc in Applied Biology, I offer a unique fusion of scientific expertise, a decade of research experience, and proficiency in coding, blending creativity and technical skills.
 As an experienced traveler, I enjoy immersing myself in local cultures and capturing their essence through photography, with a focus on nature and street scenes. As a sci-fi aficionado, I am constantly inspired by the boundless possibilities of the genre. My love for cats, particularly my two plump Siamese companions, further reflects my caring and empathetic nature.
 If you're looking for a dynamic team player, with an adventurous spirit, eager to launch himself into innovative projects and adapt to constantly changing environments, but always learning and embracing challenges, you've found the right person! I'm excited to make a real impact on projects and support the team's success. 🦄</p>
+</div>
   </div>
 </section>
-        <p className="text-center">As a Frontend Developer with an MSc in Applied Biology, I offer a unique fusion of scientific expertise, a decade of research experience, and proficiency in coding, blending creativity and technical skills.
-As an experienced traveler, I enjoy immersing myself in local cultures and capturing their essence through photography, with a focus on nature and street scenes. As a sci-fi aficionado, I am constantly inspired by the boundless possibilities of the genre. My love for cats, particularly my two plump Siamese companions, further reflects my caring and empathetic nature.
-If you're looking for a dynamic team player, with an adventurous spirit, eager to launch himself into innovative projects and adapt to constantly changing environments, but always learning and embracing challenges, you've found the right person! I'm excited to make a real impact on projects and support the team's success. 🦄</p>
-      </div>
-    </section>
   );
 };
 
