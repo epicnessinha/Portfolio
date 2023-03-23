cdcefae2ebc6f44a0778bb0c11dd3f495f2527bc
Vanessa <vanessa.ferreira.dev@gmail.com>
2023-03-23 18:31:22 +0000

    Design updates

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Carousel from '../Carousel/Carousel';
+import "./Projects.css";
 
 const Projects = () => {
   return (
     <section id="projects" className="projects-section">
       <div className="container">
         <h2 className="text-center">Projects</h2>
-        <p className="text-center">A showcase of some of my projects. You can find more details about each one them in my Github.</p>
+        <p className="text-center">A showcase of some of my projects. You can find more details about each one them in my Github page.</p>
         <Carousel />
       </div>
     </section>
