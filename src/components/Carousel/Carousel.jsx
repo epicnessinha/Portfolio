065d9fc1a7ff533eb0e78fe0a88995751afac81b
Vanessa <vanessa.ferreira.dev@gmail.com>
2023-03-28 13:49:46 +0100

    Final updates, responsive design

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -13,14 +13,16 @@ import mycartel from "../../assets/mycartel.png";
 import mycartelquest from "../../assets/mycartelquest.png"
 import petlandlogin from "../../assets/Petlandlogin.png";
 import petlandregister from "../../assets/Petlandregister.png"
+import petlandprofile from "../../assets/petlandprofile.png"
+import petlandadmn from "../../assets/petlandadmn.png"
 import "./Carousel.css";
 
 const carouselImages = [
   { src: homepage, alt: "Project 1" },
   { src: haute, alt: "Project 2" },
+  { src: geekshubstore, alt: "Project 4" },
   { src: Sims, alt: "Project 10" },
   { src: SimsChar, alt: "Project 8" },
-  { src: geekshubstore, alt: "Project 4" },
   { src: music, alt: "Project 5" },
   { src: todolist, alt: "Project 6" },
   { src: nintendo, alt: "Project 9" },
@@ -29,7 +31,9 @@ const carouselImages = [
   { src: movieland, alt: "Project 7" },
   { src: Petland, alt: "Project 3" },
   { src: petlandlogin, alt: "Project 13" },
-  { src: petlandregister, alt: "Project 14" },
+  { src: petlandprofile, alt: "Project 14" },
+  { src: petlandadmn, alt: "Project 16" },
+  { src: petlandregister, alt: "Project 17" },
   
 ];
 
