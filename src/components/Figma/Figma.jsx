8db643d3d9fa6673c061b7e5da9c348e9a7541f4
Vanessa <vanessa.ferreira.dev@gmail.com>
2023-04-16 12:50:42 +0100

    Figma compomente (UI/UX Designs) created

---
+++ src/components/Figma/Figma.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Figma.css"
+
+const Figma = () => {
+  return (
+    <section id="figma" className="figma-section">
+    <div className="figmacontainer">
+      <h2 className="figma">UI/UX Designs</h2>
+      <p className="figmatext">A showcase of some of my UI/UX Designs drafts made on Figma.</p>
+    </div>
+  </section>
+  )
+}
+
+export default Figma
